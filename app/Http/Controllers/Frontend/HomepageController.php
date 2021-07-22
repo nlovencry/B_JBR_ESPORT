8f53d92e849454e1db22ad7e0a39e30e4441995e
Fathur Rohman <fathur.rahman12300@gmail.com>
2021-07-22 12:27:40 +0700

    up

--- app/Http/Controllers/Frontend/HomepageController.php
+++ app/Http/Controllers/Frontend/HomepageController.php
@@ -11,26 +11,26 @@ use DB;
 class HomepageController extends Controller
 {
     public function index(){
-        $games = DB::table('game')->latest()->paginate(3);;
+        $games = DB::table('game')->latest()->paginate(3);
         $event = DB::table('event')
-        ->select('event.*','game.nama_game')
-        ->leftjoin('game','game.id_game','=','event.id_game')
-        ->latest()->paginate(3);
+                    ->select('event.*','game.nama_game')
+                    ->leftjoin('game','game.id_game','=','event.id_game')
+                    ->latest()->paginate(3);
         $eventfoot = Event::latest()->paginate(3);
         $player = DB::table('player')
-        ->select('player.*','users.*','game.nama_game')
-        ->leftjoin('users','users.id','=','player.id')
-        ->leftjoin('game','game.id_game','=','player.id_game')
-        ->where('users.is_active','1')
-        ->latest('player.created_at')
-        ->paginate(4);
+                    ->select('player.*','users.*','game.nama_game')
+                    ->leftjoin('users','users.id','=','player.id')
+                    ->leftjoin('game','game.id_game','=','player.id_game')
+                    ->where('users.is_active','1')
+                    ->latest('player.created_at')
+                    ->paginate(4);
         $playerfoot = DB::table('player')
-        ->select('player.*','users.*','game.nama_game')
-        ->leftjoin('users','users.id','=','player.id')
-        ->leftjoin('game','game.id_game','=','player.id_game')
-        ->where('users.is_active','1')
-        ->latest('player.created_at')
-        ->paginate(3);
+                    ->select('player.*','users.*','game.nama_game')
+                    ->leftjoin('users','users.id','=','player.id')
+                    ->leftjoin('game','game.id_game','=','player.id_game')
+                    ->where('users.is_active','1')
+                    ->latest('player.created_at')
+                    ->paginate(3);
         // dd($player);
         $team = DB::table('team')
                     ->select('team.*','game.*',DB::raw('count(player.id_player) as total_player'),)
